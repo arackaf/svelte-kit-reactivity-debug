5919172a0f0319e18f0e16db99bb3847db299331
Adam Rackis <adamrackis@hotmail.com>
2025-02-13 19:23:29 -0600

    finish demo

--- src/routes/nowork/Display.svelte
+++ src/routes/nowork/Display.svelte
@@ -6,11 +6,15 @@
 	let childrenCopy = $state(item.children);
 	let childrenTracked = $derived(item.children);
 
+	// WORKS
+	// $effect(() => {
+	// 	childrenCopy = childrenTracked;
+	// });
+
+	// NO WORK
 	$effect(() => {
 		let updatedChildren = childrenTracked;
-		let currentChildren = untrack(() => childrenCopy);
 		let currentChildrenLength = untrack(() => childrenCopy.length);
-		//console.log('Logging', { currentChildren, updatedChildren });
 
 		if (updatedChildren.length !== currentChildrenLength) {
 			childrenCopy = updatedChildren;
